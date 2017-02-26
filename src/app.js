7c5832c3f2040837ab3aff59e9db6ad978fa105a
xiao.su <xiao.su@innocellence.com>
2017-02-26 20:40:04 +0800

    no message

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@
 var express = require("express");
 var app = express();
 
+// 一个中间件栈，对任何指向 / 的 HTTP 请求打印出相关信息
+app.use('/', function(req, res, next) {
+    console.log('Request URL:', req.originalUrl);
+    next();
+}, function (req, res, next) {
+    console.log('Request Type:', req.method);
+    next();
+});
+
+//路由方法 ： homepage get请求
 app.get("/", function(req, res){
     res.send("hello world!");
 });
@@ -14,3 +24,10 @@ var server = app.listen(3007,function(){
     var port = server.address().port;
     console.log("app listening at http://%s:%s",host,port);
 });
+
+//托管静态文件，public 目录下面的文件就可以访问了
+app.use(express.static("./public"));
+/*
+http://localhost:3007/images/banner.png
+    //所有文件的路径都是相对于存放目录的，因此，存放静态文件的目录名不会出现在 URL 中。
+*/
---
+++ src/server/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ThinkPad on 2017/2/26.
+ */
+module.exports = {
+    "serverPort": 3007
+};
