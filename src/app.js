6bab99e897d2fc822bef3cbf351ca9155b90b0e7
xiao.su <xiao.su@innocellence.com>
2017-03-04 21:07:20 +0800

    homepage loginpage

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@
 
 var express = require("express");
 var app = express();
+var path = require("path");
 
 // 一个中间件栈，对任何指向 / 的 HTTP 请求打印出相关信息
 app.use('/', function(req, res, next) {
@@ -18,6 +19,17 @@ app.use('/', function(req, res, next) {
 app.get("/", function(req, res){
     res.send("hello world!");
 });
+app.get("/homepage", function(req, res){
+    // res.send("hello world!");
+    // res.sendFile("app.html",{root:path.join(__dirname,"./public")});
+    res.sendFile("app.html",{root:path.join(__dirname,"./cms")});
+});
+app.get("/login", function(req, res){
+    // res.send("hello world!");
+    // res.sendFile("app.html",{root:path.join(__dirname,"./public")});
+    res.sendFile("appWebLogIn.html",{root:path.join(__dirname,"./cms")});
+});
+
 
 var server = app.listen(3007,function(){
     var host = server.address().address;
@@ -27,6 +39,7 @@ var server = app.listen(3007,function(){
 
 //托管静态文件，public 目录下面的文件就可以访问了
 app.use(express.static("./public"));
+app.use(express.static("./cms"));
 /*
 http://localhost:3007/images/banner.png
     //所有文件的路径都是相对于存放目录的，因此，存放静态文件的目录名不会出现在 URL 中。
